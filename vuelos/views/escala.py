3c66912d73f8f41d638509e148e4783b5f47c8f7
jossu <xdearly12@gmail.com>
2026-07-16 01:24:21 -0500

    Endpoints publicos de lectura para escalas y estados de vuelo

--- vuelos/views/escala.py
+++ vuelos/views/escala.py
@@ -4,12 +4,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 from vuelos.models import Escala
 from vuelos.serializers import EscalaSerializer
 from vuelos.pagination import StandardPagination
-from vuelos.permissions import EsStaffOSoloLectura
+from vuelos.permissions import LecturaPublicaEscrituraStaff
 
 class EscalaViewSet(viewsets.ModelViewSet):
     queryset           = Escala.objects.select_related('vuelo', 'aeropuerto').all()
     serializer_class   = EscalaSerializer
-    permission_classes = [EsStaffOSoloLectura]
+    permission_classes = [LecturaPublicaEscrituraStaff]
     pagination_class   = StandardPagination
     filter_backends    = [DjangoFilterBackend, OrderingFilter]
     filterset_fields   = ['vuelo', 'aeropuerto']
--- vuelos/views/estado_vuelo.py
+++ vuelos/views/estado_vuelo.py
@@ -4,12 +4,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 from vuelos.models import EstadoVuelo
 from vuelos.serializers import EstadoVueloSerializer
 from vuelos.pagination import StandardPagination
-from vuelos.permissions import EsStaffOSoloLectura
+from vuelos.permissions import LecturaPublicaEscrituraStaff
 
 class EstadoVueloViewSet(viewsets.ModelViewSet):
     queryset           = EstadoVuelo.objects.select_related('vuelo').all()
     serializer_class   = EstadoVueloSerializer
-    permission_classes = [EsStaffOSoloLectura]
+    permission_classes = [LecturaPublicaEscrituraStaff]
     pagination_class   = StandardPagination
     filter_backends    = [DjangoFilterBackend, OrderingFilter]
     filterset_fields   = ['vuelo', 'estado']
